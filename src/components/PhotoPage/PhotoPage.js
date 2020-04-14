77c2742fd1dde76eb9a4691677cf6fe476a03bef
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-14 16:34:14 -0600

    Add actions and reducers directories and index files

--- src/components/PhotoPage/PhotoPage.js
+++ src/components/PhotoPage/PhotoPage.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class PhotoPage extends Component {
+
   render() {
     return(
       <section>
-
+        <article className="image-container">
+          <img src="" alt="" />
+        </article>
+        <article className="details-container">
+          <h2>Title</h2>
+          <h3>Date</h3>
+          <p>Description</p>
+        </article>
       </section>
     )
   }
