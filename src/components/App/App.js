b61b91f52090fb055dba92450a06aa6af532a6a0
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 14:34:05 -0600

    Add NavBar component and global font styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import PhotoPage from '../PhotoPage/PhotoPage'
+import NavBar from '../NavBar/NavBar'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <PhotoPage />
         <footer>
           <p>Created by Lili Manrique</p>
-          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, 
+          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a>,
           and <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a>from <a
               href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </footer>
