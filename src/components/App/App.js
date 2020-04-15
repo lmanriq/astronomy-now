64095e8972373366683ec4c1b99829ecfd015348
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 15:26:20 -0600

    Add basic router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import PhotoPage from '../PhotoPage/PhotoPage'
-import NavBar from '../NavBar/NavBar'
+import PhotoPage from '../PhotoPage/PhotoPage';
+import NavBar from '../NavBar/NavBar';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar /> 
-      <PhotoPage />
+      <Switch>
+          <Route
+            path="/" exact
+            component={() =>
+              <PhotoPage />
+            }
+          />
+          <Route
+            path="/login" exact
+            component={() =>
+              <LoginPage />
+            }
+          />
+        </Switch>
       <footer>
         <p>Created by Lili Manrique</p>
         <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, <a href="https://www.flaticon.com/authors/darius-dan" title="Darius Dan">Darius Dan</a>,
