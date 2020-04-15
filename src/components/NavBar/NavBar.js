a822422959dcbe45fa55118d9dcc5b914f0a1c61
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 15:23:35 -0600

    Tweak main page styles and change to column view

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,36 +7,38 @@ class NavBar extends Component {
   render() {
     return(
       <header className="flex-container">
-        <img src="/images/saturn.svg" alt="saturn logo" className="logo"/>
-        <article className="welcome-msg">
-          <h1>Astronomy Now</h1>
-          <h2>Welcome, Guest</h2>
-          <button>login</button>
+        <article className="flex-container">
+          <img src="/images/saturn.svg" alt="saturn logo" className="logo"/>
+          <div className="welcome-msg ">
+            <h1>Astronomy Now</h1>
+            <h2>Welcome, Guest</h2>
+            <button>login</button>
+          </div>
         </article>
         <nav className="flex-container">
           <div className="nav-btn-container flex-container">
+            <img src="/images/spaceship.svg" alt="spaceship icon"/>
             <button>
               <p>Photos of the Day</p>
             </button>
-            <img src="/images/spaceship.svg" alt="spaceship icon"/>
           </div>
           <div className="nav-btn-container flex-container">
+            <img src="/images/iss.svg" alt="ISS icon"/>
             <button>
               <p>ISS Tracking</p>
             </button>
-            <img src="/images/iss.svg" alt="ISS icon"/>
           </div>
           <div className="nav-btn-container flex-container">
+            <img src="/images/hubble.svg" alt="Hubble telescope icon"/>
             <button>
               <p>Hubble News</p>
             </button>
-            <img src="/images/hubble.svg" alt="Hubble telescope icon"/>
           </div>
           <div className="nav-btn-container flex-container">
+            <img src="/images/alien.svg" alt="alien with heart eyes icon"/>
             <button>
               <p>Favorites</p>
             </button>
-            <img src="/images/alien.svg" alt="alien with heart eyes icon"/>
           </div>
         </nav>
       </header>
