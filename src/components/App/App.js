303fd41987d97db6c02091b182fb325628172f74
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 11:00:52 -0600

    Move footer from index.html into app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,12 @@ function App() {
       <header className="App-header">
       </header>
       <PhotoPage />
+      <footer>
+        <p>Created by Lili Manrique</p>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, 
+        and <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a>from <a
+            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </footer>
     </div>
   );
 }
