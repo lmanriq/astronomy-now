472a7085b3b224d97c9144e0fbbf4f79d7e38afa
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 11:27:30 -0600

    Add fixed logo to upper lefthand corner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,16 @@ import PhotoPage from '../PhotoPage/PhotoPage'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <PhotoPage />
-      <footer>
-        <p>Created by Lili Manrique</p>
-        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, 
-        and <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a>from <a
-            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-      </footer>
+      <img src="/images/saturn.svg" alt="saturn logo" className="logo"/>
+      <section className="main-page">
+        <PhotoPage />
+        <footer>
+          <p>Created by Lili Manrique</p>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a>, 
+          and <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a>from <a
+              href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </footer>
+      </section>
     </div>
   );
 }
