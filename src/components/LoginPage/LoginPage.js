ae3b6a9a72952827b837974b65a4ce292a17ed6a
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 17:12:57 -0600

    Write out basic render

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./LoginPage.css";
+import LoginForm from "../LoginForm/LoginForm";
 
 const LoginPage = () => {
   return(
-    <section className="main-page">
-
+    <section className="main-page flex-container">
+      <section className="logo-container flex-container">
+        <h1>Astronomy Now</h1>
+        <img src="/images/saturn.svg" alt="saturn logo"/>
+      </section>
+      <section className="form-container flex-container">
+        <h2>Stay in the orbit of what’s happening today in space</h2>
+        <LoginForm />
+      </section>
     </section>
   )
 }
