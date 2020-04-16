fc5dc1483299bebe78f247e911af99b2c7cfba01
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 14:08:55 -0600

    Use NASA API key to see if that helps with running out of fetches

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,6 @@ export const HUBBLE_BASE = 'https://hubblesite.org/api/v3';
 
 export const HUBBLE_NEWS_ENDPOINT = '/news';
 
-export const HUBBLE_SPECIFIC_STORY_ENDPOINT = (news_id) => `/news_release/${news_id}`;
+export const HUBBLE_SPECIFIC_STORY_ENDPOINT = (news_id) => `/news_release/${news_id}?api_key=K8bT9srIhxHahbiQkbH7EvTichFoJBi02JzMR5Ys`;
 
 export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
