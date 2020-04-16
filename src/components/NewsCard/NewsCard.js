de2f2f30a17b6330bf939b5f578730f51b4b0813
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 14:34:34 -0600

    Adjust initial styling for text scrolling and to display how to read more

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import './NewsCard.css'
+import React from "react";
+import "./NewsCard.css";
 
-const NewsCard = (props) => {
-  const { title, url, image, description, date} = props;
+const NewsCard = props => {
+  const { title, url, image, description, date, index } = props;
 
-  return(
-    <article className="news-card">
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <div className="card-image-container">
-          <img src={image} alt={title}/>
-        </div>
-        <div className="text-container">
+  return (
+    <section>
+      <article className="news-card">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <div className="card-image-container">
+            <img src={image} alt={title} />
+            <p>Click to Read More</p>
+          </div>
+        </a>
+        <div className="card-text-container">
           <h1>{title}</h1>
           <p>{date}</p>
           <p>{description}</p>
         </div>
-      </a>
-    </article>
-  )
-}
+      </article>
+    </section>
+  );
+};
 
 export default NewsCard;
