fbcedad836e253ef96f96e22e835d3f405d7a8b1
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 15:26:20 -0600

    Add favorites to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { photoOfTheDay } from './photoOfTheDay';
 import { user } from './user';
 import { news } from './news';
+import { favorites } from './favorites';
 
 const rootReducer = combineReducers({
   photoOfTheDay,
   user,
-  news
+  news,
+  favorites
 })
 
 export default rootReducer;
