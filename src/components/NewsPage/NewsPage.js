dc583f64ff5f7a16ce4aad506fe57ae9b2967319
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 10:01:23 -0600

    Write basic render and fetch news data

--- src/components/NewsPage/NewsPage.js
+++ src/components/NewsPage/NewsPage.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { HUBBLE_BASE, HUBBLE_ALL_NEWS_ENDPOINT, HUBBLE_SPECIFIC_STORY_ENDPOINT } from "../../utils/constants";
 
 
 class NewsPage extends Component {
+  async componentDidMount() {
+    const newsResponse = await fetch(HUBBLE_BASE + HUBBLE_ALL_NEWS_ENDPOINT);
+    const newsData = await newsResponse.json();
+    console.log(newsData)
+  } 
 
+  render() {
+    return(
+      <section className="news-page main-page">
+        <h1>News from the Hubble Space Telescope</h1>
+      </section>
+    )
+  }
 }
 
 export default NewsPage; 
