a003a5bbd64fe2d209de7567ca94afe845dd52d7
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 20:37:19 -0600

    Add user to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { photoOfTheDay } from './photoOfTheDay';
+import { user } from './user';
 
 const rootReducer = combineReducers({
-  photoOfTheDay
+  photoOfTheDay,
+  user
 })
 
 export default rootReducer;
