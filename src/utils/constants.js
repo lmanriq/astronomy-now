3a42b7943723ade09662540ab85c56d1e7ccacdd
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 10:00:34 -0600

    Add Hubble news base and endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,7 @@
 export const POTD_URL = 'https://api.nasa.gov/planetary/apod?api_key=K8bT9srIhxHahbiQkbH7EvTichFoJBi02JzMR5Ys';
+
+export const HUBBLE_BASE = 'https://hubblesite.org/api/v3';
+
+export const HUBBLE_ALL_NEWS_ENDPOINT = '/news?page=all';
+
+export const HUBBLE_SPECIFIC_STORY_ENDPOINT = (news_id) => `/api/v3/news_release/${news_id}`
