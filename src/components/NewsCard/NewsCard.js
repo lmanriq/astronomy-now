f7a36e69d18323739f57449d3745967a92ba20db
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 14:51:47 -0600

    Add favorites box

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./NewsCard.css";
 
 const NewsCard = props => {
-  const { title, url, image, description, date, index } = props;
+  const { title, url, image, description, date} = props;
 
   return (
-    <section>
+    <section className="article-container flex-container">
       <article className="news-card">
         <a href={url} target="_blank" rel="noopener noreferrer">
           <div className="card-image-container">
@@ -19,6 +19,10 @@ const NewsCard = props => {
           <p>{description}</p>
         </div>
       </article>
+      <div className="favorites-box flex-container">
+        <img className="heart-icon" src="/images/empty-heart.svg" alt="heart icon" />
+        <p>Add to Favorites</p>
+      </div>
     </section>
   );
 };
