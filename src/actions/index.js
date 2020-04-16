1543912dfef2177c14cbfe058ac423f305a80e47
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-15 20:27:30 -0600

    Add login and logout action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,12 @@ export const loadPhotoOfTheDay = data => ({
   type: 'LOAD_POTD',
   data
 })
+
+export const loginUser = user => ({
+  type: 'LOGIN_USER',
+  user
+})
+
+export const logoutUser = user => ({
+  type: 'LOGOUT_USER'
+})
