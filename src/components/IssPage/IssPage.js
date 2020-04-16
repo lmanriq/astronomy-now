890f1745da97394cf1e5c02875431730c5da1fd6
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 17:23:00 -0600

    Begin people in space loading

--- src/components/IssPage/IssPage.js
+++ src/components/IssPage/IssPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./IssPage.css";
+import { updateISSPosition, updatePeople } from "../../actions";
+import { connect } from "react-redux";
 import {
   ISS_BASE,
   ISS_NOW_ENDPOINT,
@@ -8,20 +10,27 @@ import {
 
 class IssPage extends Component {
   async componentDidMount() {
-    const nowResponse = await fetch(ISS_BASE + ISS_NOW_ENDPOINT);
-    const nowData = await nowResponse.json();
-    console.log(nowData.iss_position);
-
+    const peopleResponse = await fetch(ISS_BASE + ISS_PEOPLE_ENDPOINT);
+    const peopleData = await peopleResponse.json();
+    this.props.updatePeople(peopleData);
+    // const updatePosition = async () => {
+    //   const nowResponse = await fetch(ISS_BASE + ISS_NOW_ENDPOINT);
+    //   const nowData = await nowResponse.json();
+    //   this.props.updateISSPosition(nowData.iss_position);
+    //   setTimeout(updatePosition, 5000);
+    // };
+    // updatePosition();
   }
 
   render() {
+    const { issPosition } = this.props;
     return (
       <section className="iss-page main-page">
         <div className="iss-map-container"></div>
         <div className="iss-form-container">
           <p>
-            The International Space Station is currently over lattitude,
-            longitude
+            The International Space Station is currently over {issPosition.latitude},
+            {issPosition.longitude}
           </p>
           <p>There are currently 6 humans in space</p>
         </div>
@@ -32,10 +41,10 @@ class IssPage extends Component {
 
 const mapStateToProps = state => ({
   issPosition: state.issPosition
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   updateISSPosition: position => dispatch(updateISSPosition(position))
-})
+});
 
-export default IssPage;
+export default connect(mapStateToProps, mapDispatchToProps)(IssPage);
