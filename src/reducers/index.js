2b409fe8f5a43fb13d0f92c098d1e2a782b01646
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 11:47:42 -0600

    Add news to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { photoOfTheDay } from './photoOfTheDay';
 import { user } from './user';
+import { news } from './news';
 
 const rootReducer = combineReducers({
   photoOfTheDay,
-  user
+  user,
+  news
 })
 
 export default rootReducer;
