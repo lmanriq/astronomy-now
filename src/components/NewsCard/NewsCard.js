ec3b8b1363aadc67f2c6166acbe9bb4864e716e6
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-16 14:07:56 -0600

    Adjust dimensions of story cards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import './NewsCard.css'
 
 const NewsCard = (props) => {
-  const { title, url, image, description} = props;
+  const { title, url, image, description, date} = props;
 
   return(
-    <article>
-      <a href={url}>
-        <h1>{title}</h1>
-        <img src={image} alt={title}/>
-        <p>{description}</p>
+    <article className="news-card">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <div className="card-image-container">
+          <img src={image} alt={title}/>
+        </div>
+        <div className="text-container">
+          <h1>{title}</h1>
+          <p>{date}</p>
+          <p>{description}</p>
+        </div>
       </a>
     </article>
   )
