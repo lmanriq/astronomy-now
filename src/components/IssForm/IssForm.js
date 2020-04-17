a2c32592c55d4b4286aa4f724b69a2f741535a0c
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-17 10:35:28 -0600

    Style form and display list

--- src/components/IssForm/IssForm.js
+++ src/components/IssForm/IssForm.js
@@ -4,6 +4,7 @@ import {
   ISS_PASSTIMES_ENDPOINT,
   PROXY_URL
 } from "../../utils/constants";
+import './IssForm.css'
 import { connect } from "react-redux";
 import { loadSearchResults } from "../../actions";
 
@@ -27,11 +28,16 @@ class IssForm extends Component {
     const { lat, lon } = this.state;
     console.log(lat, lon);
     if (lat < 80 && lat > -80 && lon < 180 && lon > -180) {
-      const passResponse = await fetch(
-        PROXY_URL + ISS_BASE + ISS_PASSTIMES_ENDPOINT(lat, lon)
-      );
-      const passData = await passResponse.json();
-      this.props.loadSearchResults(passData.response);
+      try {
+        const passResponse = await fetch(
+          PROXY_URL + ISS_BASE + ISS_PASSTIMES_ENDPOINT(lat, lon)
+        );
+        const passData = await passResponse.json();
+        this.props.loadSearchResults(passData.response);
+      }
+      catch (error) {
+        console.error(error.message)
+      }
     }
   }
 
@@ -46,26 +52,32 @@ class IssForm extends Component {
     ));
     return (
       <section className="form-section">
-        <form>
-          <h2>When will the ISS pass over me?</h2>
-          <label htmlFor="lat">latitude (-80 to 80):</label>
-          <input
-            onChange={e => this.handleChange(e)}
-            id="lat"
-            type="number"
-            min="-80"
-            max="80"
-            value={lat}
-          />
-          <label htmlFor="lon">longitude (-180 to 180):</label>
-          <input
-            onChange={e => this.handleChange(e)}
-            id="lon"
-            type="number"
-            min="-180"
-            max="180"
-            value={lon}
-          />
+        <h2>When will the ISS pass over me?</h2>
+        <form className="flex-container">
+          <div className="inputs-wrapper flex-container">
+            <div className="input-container flex-container">
+              <label htmlFor="lat">latitude (-80 to 80):</label>
+              <input
+                onChange={e => this.handleChange(e)}
+                id="lat"
+                type="number"
+                min="-80"
+                max="80"
+                value={lat}
+              />
+            </div>
+            <div className="input-container flex-container">
+              <label htmlFor="lon">longitude (-180 to 180):</label>
+              <input
+                onChange={e => this.handleChange(e)}
+                id="lon"
+                type="number"
+                min="-180"
+                max="180"
+                value={lon}
+              />
+            </div>
+          </div>
           <button
             onClick={this.searchPassover.bind(this)}
             type="button"
