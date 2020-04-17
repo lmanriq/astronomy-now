edf5dbdd48937f3abeee16e1f1efb0c81805f3e7
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-17 14:58:18 -0600

    Break out fetch functions into API calls file

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,7 +1,9 @@
 import {
   ISS_BASE,
   ISS_PASSTIMES_ENDPOINT,
-  PROXY_URL
+  PROXY_URL,
+  ISS_NOW_ENDPOINT,
+  ISS_PEOPLE_ENDPOINT
 } from "../utils/constants";
 
 export const fetchPasstimes = async (lat, lon) => {
@@ -10,4 +12,16 @@ export const fetchPasstimes = async (lat, lon) => {
   );
   const passData = await passResponse.json();
   return passData;
+};
+
+export const fetchPosition = async () => {
+  const nowResponse = await fetch(ISS_BASE + ISS_NOW_ENDPOINT);
+  const nowData = await nowResponse.json();
+  return nowData;
+};
+
+export const fetchPeopleData = async () => {
+  const peopleResponse = await fetch(ISS_BASE + ISS_PEOPLE_ENDPOINT);
+  const peopleData = await peopleResponse.json();
+  return peopleData;
 };
