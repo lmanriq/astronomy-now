016443d39bf546155bdbcf92eb6ccc8b22289a2c
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-17 15:18:45 -0600

    Write login form render test

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import LoginForm from './LoginForm';
+import React from "react";
+import { render } from "@testing-library/react";
+import LoginForm from "./LoginForm";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../../reducers';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../../reducers";
 
-describe('Login Form ', () => {
-  it('Should render what we expect', () => {
-
-  })
-})
+describe("Login Form ", () => {
+  it("Should render what we expect", () => {
+    const store = createStore(rootReducer);
+    const { getByText, getByPlaceholderText } = render(
+      <Provider store={store}>
+        <Router>
+          <LoginForm />
+        </Router>
+      </Provider>
+    );
+    expect(getByText("First Name:")).toBeInTheDocument();
+    expect(getByText("Email:")).toBeInTheDocument();
+    expect(getByPlaceholderText("your name")).toBeInTheDocument();
+    expect(getByPlaceholderText("email")).toBeInTheDocument();
+  });
+});
