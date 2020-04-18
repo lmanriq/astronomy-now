77be4064c9e16db1e38c6bbcdaa2090288e26311
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-18 15:04:55 -0600

    Updates

---
+++ precache-manifest.8657634bc808b5b32afb97f5f13dc8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3be7e74cbf242bbff6458faaee045c2",
+    "url": "/astronomy-now/index.html"
+  },
+  {
+    "revision": "f682dc124b5ec177ebf4",
+    "url": "/astronomy-now/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "df04d57a6b10dc04fa31",
+    "url": "/astronomy-now/static/css/main.2dce7876.chunk.css"
+  },
+  {
+    "revision": "f682dc124b5ec177ebf4",
+    "url": "/astronomy-now/static/js/2.f6073c18.chunk.js"
+  },
+  {
+    "revision": "aa10947fa12f70ba9a1c6037c8af9e03",
+    "url": "/astronomy-now/static/js/2.f6073c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df04d57a6b10dc04fa31",
+    "url": "/astronomy-now/static/js/main.5856d91e.chunk.js"
+  },
+  {
+    "revision": "6484726d2deefb8272c4",
+    "url": "/astronomy-now/static/js/runtime-main.3c7d28bf.js"
+  }
+]);
