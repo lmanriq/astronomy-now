9deb4c99906145008b84a779dc67c3c7057bc67c
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-18 14:51:22 -0600

    Center image and adjust modal styles for expanded modal

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
+import './PhotoCard.css'
 
 class PhotoCard extends Component {
   constructor() {
@@ -22,14 +23,14 @@ class PhotoCard extends Component {
 
   render () {
     return (
-      <div>
+      <div className="rover-photo-container"> 
         <img className="rover-photo" src={this.props.image} onClick={this.handleOpenModal} alt="from the curiosity rover" />
         <Modal 
            isOpen={this.state.showModal}
            contentLabel="expanded photo"
         >
           <button onClick={this.handleCloseModal}>x</button>
-          <img src={this.props.image} alt="from the curiosity rover" />
+          <img className="expanded-rover-photo" src={this.props.image} alt="from the curiosity rover" />
         </Modal>
       </div>
     );
