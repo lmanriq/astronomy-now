5d0d0dbf8be98e9e70f8e62e0843864b156fca9a
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 15:47:36 -0600

    Add loading page to ISS

--- src/components/IssPage/IssPage.js
+++ src/components/IssPage/IssPage.js
@@ -1,27 +1,34 @@
 import React, { Component } from "react";
 import "./IssPage.css";
-import { updateISSPosition, loadPeople } from "../../actions";
+import { updateISSPosition, loadPeople, updateLoading } from "../../actions";
 import { connect } from "react-redux";
 import IssMap from "../IssMap/IssMap";
 import IssForm from "../IssForm/IssForm";
 import { fetchPosition, fetchPeopleData } from "../../utils/apiCalls";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
+import LoadingPage from "../LoadingPage/LoadingPage";
 
 class IssPage extends Component {
   async componentDidMount() {
-    const peopleData = await fetchPeopleData();
-    this.props.loadPeople(peopleData);
-    const updatePosition = async () => {
-      const nowData = await fetchPosition();
-      this.props.updateISSPosition(nowData.iss_position);
-      setTimeout(updatePosition, 5000);
-    };
-    updatePosition();
+    try {
+      this.props.updateLoading(true);
+      const peopleData = await fetchPeopleData();
+      this.props.loadPeople(peopleData);
+      const updatePosition = async () => {
+        const nowData = await fetchPosition();
+        this.props.updateISSPosition(nowData.iss_position);
+        setTimeout(updatePosition, 5000);
+      };
+      updatePosition();
+      this.props.updateLoading(false);
+    } catch (error) {
+      this.props.updateLoading(false);
+      console.error(error.message);
+    }
   }
 
   render() {
-    const { issPosition, peopleData } = this.props;
+    const { issPosition, peopleData, isLoading } = this.props;
     const { latitude, longitude } = issPosition;
     const peopleList = peopleData.people
       ? peopleData.people.map((person, index) => (
@@ -30,34 +37,40 @@ class IssPage extends Component {
           </li>
         ))
       : "loading...";
-    return (
-      <section className="iss-page main-page flex-container">
-        <div className="iss-map-container flex-container">
-          <IssMap />
-        </div>
-        <div className="iss-form-container flex-container">
-          <h1>International Space Station Tracking</h1>
-          <p>
-            The ISS is currently over{" "}
-            {latitude} {latitude > 0 ? 'N' : 'S'}, {longitude} {longitude > 0 ? 'E' : 'W'}
-          </p>
-          <p>There are currently {peopleData.number} humans in space</p>
-          <ul>{peopleList}</ul>
-          <IssForm />
-        </div>
-      </section>
-    );
+    if(isLoading) {
+      return <LoadingPage />
+    } else {
+      return (
+        <section className="iss-page main-page flex-container">
+          <div className="iss-map-container flex-container">
+            <IssMap />
+          </div>
+          <div className="iss-form-container flex-container">
+            <h1>International Space Station Tracking</h1>
+            <p>
+              The ISS is currently over {latitude} {latitude > 0 ? "N" : "S"},{" "}
+              {longitude} {longitude > 0 ? "E" : "W"}
+            </p>
+            <p>There are currently {peopleData.number} humans in space</p>
+            <ul>{peopleList}</ul>
+            <IssForm />
+          </div>
+        </section>
+      );
+    }
   }
 }
 
 const mapStateToProps = state => ({
   issPosition: state.issPosition,
-  peopleData: state.peopleData
+  peopleData: state.peopleData,
+  isLoading: state.isLoading
 });
 
 const mapDispatchToProps = dispatch => ({
   updateISSPosition: position => dispatch(updateISSPosition(position)),
-  loadPeople: peopleData => dispatch(loadPeople(peopleData))
+  loadPeople: peopleData => dispatch(loadPeople(peopleData)),
+  updateLoading: loading => dispatch(updateLoading(loading))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(IssPage);
@@ -66,5 +79,5 @@ IssPage.propTypes = {
   issPosition: PropTypes.object,
   peopleData: PropTypes.object,
   updateISSPosition: PropTypes.func,
-  loadPeople: PropTypes.func,
-}
+  loadPeople: PropTypes.func
+};
