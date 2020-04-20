ba95b63a90d0d46640579c285fec9604f2d8b993
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 14:02:14 -0600

    Add favorites button and functionality

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,40 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
-import './PhotoCard.css'
+import Modal from "react-modal";
+import "./PhotoCard.css";
+import { connect } from "react-redux";
+import { addFavorite, removeFavorite } from "../../actions";
+import PropTypes from 'prop-types';
 
 class PhotoCard extends Component {
   constructor() {
     super();
     this.state = {
       showModal: false
-    }
+    };
     this.handleOpenModal = this.handleOpenModal.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
-  handleOpenModal () {
+  handleOpenModal() {
     this.setState({ showModal: true });
   }
-  
-  handleCloseModal () {
+
+  handleCloseModal() {
     this.setState({ showModal: false });
   }
 
-  render () {
-    return (
-      <button className="rover-photo-container" data-testid={this.props.testid}> 
-        <img className="rover-photo" src={this.props.image} onClick={this.handleOpenModal} alt="from the curiosity rover" />
-        <Modal 
-           isOpen={this.state.showModal}
-           contentLabel="expanded photo"
-        >
-          <button data-testid="close-image-btn" onClick={this.handleCloseModal}>x</button>
-          <img className="expanded-rover-photo" src={this.props.image} alt="from the curiosity rover - expanded" />
-        </Modal>
+  render() {
+    const { id, image, testid, favorites, addFavorite, removeFavorite } = this.props;
+    const favBtn = (
+      <button onClick={() => addFavorite(id)}>
+        <img
+          data-testid={`news-${id}`}
+          className="heart-icon"
+          src="/astronomy-now/images/empty-heart.svg"
+          alt="empty heart icon"
+        />
       </button>
+    );
+    const unFavBtn = (
+      <button onClick={() => removeFavorite(id)}>
+        <img
+          data-testid={`news-${id}`}
+          className="heart-icon"
+          src="/astronomy-now/images/full-heart.svg"
+          alt="full heart icon"
+        />
+      </button>
+    );
+
+    const btn = favorites.includes(id) ? unFavBtn : favBtn;
+
+    return (
+      <article className="photo-card">
+        <button className="rover-photo-container" data-testid={testid}>
+          <img
+            className="rover-photo"
+            src={image}
+            onClick={this.handleOpenModal}
+            alt="from the curiosity rover"
+          />
+          <Modal isOpen={this.state.showModal} contentLabel="expanded photo">
+            <button
+              data-testid="close-image-btn"
+              onClick={this.handleCloseModal}
+            >
+              x
+            </button>
+            <img
+              className="expanded-rover-photo"
+              src={image}
+              alt="from the curiosity rover - expanded"
+            />
+          </Modal>
+        </button>
+        <div className="favorites-box flex-container">
+          {btn}
+          <p>Add to Favorites</p>
+        </div>
+      </article>
     );
   }
 }
 
-export default PhotoCard;
+const mapStateToProps = state => ({
+  favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch => ({
+  addFavorite: id => dispatch(addFavorite(id)),
+  removeFavorite: id => dispatch(removeFavorite(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhotoCard);
