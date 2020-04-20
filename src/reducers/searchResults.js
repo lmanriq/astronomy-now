d44568b326c4648d45531ec592df133b55143afc
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 13:25:07 -0600

    Write reducer tests for searchResults

--- src/reducers/searchResults.js
+++ src/reducers/searchResults.js
@@ -1,8 +1,8 @@
 export const searchResults = (state = [], action) => {
   switch (action.type) {
-    case 'LOAD_RESULTS':
+    case "LOAD_RESULTS":
       return [...action.results];
     default:
       return state;
   }
-}
+};
---
+++ src/reducers/searchResults.test.js
@@ -0,0 +1,50 @@
+import { searchResults } from './searchResults';
+
+describe('searchResults', () => {
+  it("should return the initial state", () => {
+    const expectedResult = [];
+    const result = searchResults(undefined, ["passover"]);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("when receiving LOAD_PEOPLE action, it should return the updated object", () => {
+    const mockPassover = {
+      message: "success",
+      request: {
+        altitude: 100,
+        datetime: 1587153841,
+        latitude: 7,
+        longitude: 80,
+        passes: 5
+      },
+      response: [
+        {
+          duration: 516,
+          risetime: 1587165186
+        },
+        {
+          duration: 621,
+          risetime: 1587170929
+        },
+        {
+          duration: 615,
+          risetime: 1587206735
+        },
+        {
+          duration: 522,
+          risetime: 1587212578
+        },
+        {
+          duration: 345,
+          risetime: 1587248845
+        }
+      ]
+    };
+    const sampleAction = {
+      type: "LOAD_RESULTS",
+      results: mockPassover.response
+    };
+    const result = searchResults([], sampleAction);
+    expect(result).toEqual(mockPassover.response);
+  });
+})
