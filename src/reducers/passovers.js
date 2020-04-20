3e973405a86f53d727530b6b17019c8b2627049f
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 13:43:06 -0600

    Remove redundance passovers reducer files and remove from root reducer

--- src/reducers/passovers.js
+++
@@ -1,8 +0,0 @@
-export const passovers = (state = [], action) => {
-  switch (action.type) {
-    case "LOAD_PASSOVERS":
-      return [...action.passovers];
-    default:
-      return state;
-  }
-};
