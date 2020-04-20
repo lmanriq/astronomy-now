354ecf4086510af79e41cebe2c6c98728ab831e1
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 14:19:38 -0600

    Add photo cards to render for favs

--- src/components/FavoritesPage/FavoritesPage.js
+++ src/components/FavoritesPage/FavoritesPage.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 import NewsCard from "../NewsCard/NewsCard";
-import PropTypes from 'prop-types';
+import PhotoCard from "../PhotoCard/PhotoCard";
+import PropTypes from "prop-types";
 
-const FavoritesPage = (props) => {
-  const { favorites, news } = props;
-  const favoriteStories = news.filter(story => favorites.includes(story.news_id))
+const FavoritesPage = props => {
+  const { favorites, news, roverPhotos } = props;
+  const favoriteStories = news.filter(story =>
+    favorites.includes(story.news_id)
+  );
+  const favoritePhotos = roverPhotos.filter(photo => favorites.includes(photo.id));
   const newsCards = favoriteStories.map(story => {
     return (
       <NewsCard
@@ -19,21 +23,37 @@ const FavoritesPage = (props) => {
       />
     );
   });
+  const photoCards = favoritePhotos.map((photo, index) => (
+    <PhotoCard
+      image={photo.img_src}
+      key={index}
+      testid={photo.id}
+      id={photo.id}
+    />
+  ));
 
-  return(
+  return (
     <section className="news-page main-page flex-container">
-      <h1>Your Favorite Articles</h1>
-      {!favorites.length && <h2>You have no favorited articles yet!</h2>}
+      <h1>Your Favorites</h1>
+      {!favorites.length && (
+        <h2>You have no favorited articles or photos yet!</h2>
+      )}
+      {newsCards.length > 0 && <h3>Your favorite articles</h3>}
       <div className="news-container">
         {!!favorites.length && newsCards}
       </div>
+      {photoCards.length > 0 && <h3>Your favorite photos</h3>}
+      <div className="rover-photos-container">
+        {!!favorites.length && photoCards}
+      </div>
     </section>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   favorites: state.favorites,
-  news: state.news
+  news: state.news,
+  roverPhotos: state.roverPhotos
 });
 
 export default connect(mapStateToProps, null)(FavoritesPage);
@@ -41,4 +61,4 @@ export default connect(mapStateToProps, null)(FavoritesPage);
 FavoritesPage.propTypes = {
   favorites: PropTypes.array,
   news: PropTypes.array
-}
+};
