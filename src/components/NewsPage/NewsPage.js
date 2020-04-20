7829063e3fdd5ab5fdb0a03d3270001d42b92b67
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 15:47:13 -0600

    Add loading page

--- src/components/NewsPage/NewsPage.js
+++ src/components/NewsPage/NewsPage.js
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { loadNews } from "../../actions";
+import { loadNews, updateLoading } from "../../actions";
 import NewsCard from "../NewsCard/NewsCard";
+import LoadingPage from "../LoadingPage/LoadingPage";
 import "./NewsPage.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { fetchAllNews, fetchNewsDetails } from "../../utils/apiCalls";
-
 
 class NewsPage extends Component {
   async componentDidMount() {
     try {
+      this.props.updateLoading(true);
       const newsData = await fetchAllNews();
       this.props.loadNews(newsData);
       const allData = await fetchNewsDetails(newsData);
       this.props.loadNews(allData);
+      this.props.updateLoading(false);
     } catch (error) {
+      this.props.updateLoading(false);
       console.error(error.message);
     }
   }
 
   render() {
-    const { news } = this.props;
+    const { news, isLoading } = this.props;
     const newsCards = news.map(story => {
       return (
         <NewsCard
@@ -34,22 +37,27 @@ class NewsPage extends Component {
         />
       );
     });
-
-    return (
-      <section className="news-page main-page flex-container">
-        <h1>News from the Hubble Space Telescope</h1>
-        <div className="news-container">{newsCards}</div>
-      </section>
-    );
+    if (isLoading) {
+      return <LoadingPage />;
+    } else {
+      return (
+        <section className="news-page main-page flex-container">
+          <h1>News from the Hubble Space Telescope</h1>
+          <div className="news-container">{newsCards}</div>
+        </section>
+      );
+    }
   }
 }
 
 const mapStateToProps = state => ({
-  news: state.news
+  news: state.news,
+  isLoading: state.isLoading
 });
 
 const mapDispatchToProps = dispatch => ({
-  loadNews: news => dispatch(loadNews(news))
+  loadNews: news => dispatch(loadNews(news)),
+  updateLoading: loading => dispatch(updateLoading(loading))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewsPage);
@@ -57,4 +65,4 @@ export default connect(mapStateToProps, mapDispatchToProps)(NewsPage);
 NewsPage.propTypes = {
   new: PropTypes.array,
   loadNews: PropTypes.func
-}
+};
