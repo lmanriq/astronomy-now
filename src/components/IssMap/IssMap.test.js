fb9a2a5e646f2ad8ba794677f2ff3dad45065ca8
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 11:24:38 -0600

    Break out universal consts

--- src/components/IssMap/IssMap.test.js
+++ src/components/IssMap/IssMap.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import IssMap from './IssMap';
+import React from "react";
+import { render } from "@testing-library/react";
+import IssMap from "./IssMap";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../../reducers';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../../reducers";
 
-describe('ISS Map', () => {
-  it('Should render what we expect', () => {
+describe("ISS Map", () => {
+  it("Should render what we expect", () => {
     const store = createStore(rootReducer);
     render(
       <Provider store={store}>
@@ -16,5 +16,5 @@ describe('ISS Map', () => {
         </Router>
       </Provider>
     );
-  })
-})
+  });
+});
