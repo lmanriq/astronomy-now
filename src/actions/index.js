73c5f375d720ed915973eaa05e248843a39efa2b
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 11:50:44 -0600

    Add all action creator tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,62 +1,57 @@
 export const loadPhotoOfTheDay = data => ({
-  type: 'LOAD_POTD',
+  type: "LOAD_POTD",
   data
-})
+});
 
 export const loginUser = user => ({
-  type: 'LOGIN_USER',
+  type: "LOGIN_USER",
   user
-})
+});
 
 export const logoutUser = user => ({
-  type: 'LOGOUT_USER'
-})
+  type: "LOGOUT_USER"
+});
 
 export const loadNews = news => ({
-  type: 'LOAD_NEWS',
+  type: "LOAD_NEWS",
   news
-})
+});
 
 export const addFavorite = id => ({
-  type: 'ADD_FAVORITE',
+  type: "ADD_FAVORITE",
   id
-})
+});
 
 export const removeFavorite = id => ({
-  type: 'REMOVE_FAVORITE',
+  type: "REMOVE_FAVORITE",
   id
-})
+});
 
 export const updateISSPosition = position => ({
-  type: 'UPDATE_ISS_POSITION',
+  type: "UPDATE_ISS_POSITION",
   position
-})
+});
 
 export const loadPeople = peopleData => ({
-  type: 'LOAD_PEOPLE',
+  type: "LOAD_PEOPLE",
   peopleData
-})
-
-export const loadPassovers = passovers => ({
-  type: 'LOAD_PASSOVERS',
-  passovers
-})
+});
 
 export const loadSearchResults = results => ({
-  type: 'LOAD_RESULTS',
+  type: "LOAD_RESULTS",
   results
-})
+});
 
 export const showError = error => ({
-  type: 'SHOW_ERROR',
+  type: "SHOW_ERROR",
   error
-})
+});
 
 export const removeError = () => ({
-  type: 'REMOVE_ERROR'
-})
+  type: "REMOVE_ERROR"
+});
 
 export const loadRoverPhotos = photos => ({
-  type: 'LOAD_ROVER_PHOTOS',
+  type: "LOAD_ROVER_PHOTOS",
   photos
-})
+});
