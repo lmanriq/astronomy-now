5a03634b20c2da4d6a6896359b2044ecf8f3d15c
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-20 08:34:51 -0600

    Add empty test

---
+++ src/components/PhotoCard/PhotoCard.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import PhotoCard from './PhotoCard';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
+
+describe('Photo Card', () => {
+  it('Should render what we expect', () => {
+
+  })
+})
