597040f15a979326e310d53a5c7d0bac8ea9b01a
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-28 15:22:23 -0600

    Change API key to git ignored env variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,17 @@
 export const NASA_BASE = "https://api.nasa.gov";
 
 export const ROVER_ENDPOINT = date =>
-  `/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=K8bT9srIhxHahbiQkbH7EvTichFoJBi02JzMR5Ys`;
+  `/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${process.env.REACT_APP_API_KEY}`;
 
-export const POTD_URL =
-  "/planetary/apod?api_key=K8bT9srIhxHahbiQkbH7EvTichFoJBi02JzMR5Ys";
+export const POTD_URL = 
+  `/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`;
 
 export const HUBBLE_BASE = "https://hubblesite.org/api/v3";
 
 export const HUBBLE_NEWS_ENDPOINT = "/news";
 
 export const HUBBLE_SPECIFIC_STORY_ENDPOINT = news_id =>
-  `/news_release/${news_id}?api_key=K8bT9srIhxHahbiQkbH7EvTichFoJBi02JzMR5Ys`;
+  `/news_release/${news_id}?api_key=${process.env.REACT_APP_API_KEY}`;
 
 export const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
